966d831c85136b5891eb5541da74e4c5bb542aa7
Tiago <tiagonuwe@hotmail.com>
2015-08-24 21:04:50 -0300

    projeto tc
    tt

---
+++ src/Teclado.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Tiago
+ * Date: 24/08/15
+ * Time: 20:21
+ * To change this template use File | Settings | File Templates.
+ */
+public class Teclado {
+}
---
+++ src/testarTec.java
@@ -0,0 +1,15 @@
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Tiago
+ * Date: 24/08/15
+ * Time: 20:28
+ * To change this template use File | Settings | File Templates.
+ */
+public class testarTec {
+    public static void main(String[] args){
+         Teclado tc= new Teclado();
+         tc.lerInt();
+
+    }
+}
